27b134f8cb5fbaabcebb6dfc507b61b71e997702
nya282828 <cat.route20@gmail.com>
2018-04-29 20:59:09 +0900

    setting

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <router-view/> -->
+    <div class="sample">
+      <p class="sample__text">Hellow World</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.sample {
+  width: 100%;
+  max-width: 640px;
+  margin: 0 auto;
+
+  &__text {
+    font-size: 32px;
+    text-align: center;
+  }
+}
+</style>
